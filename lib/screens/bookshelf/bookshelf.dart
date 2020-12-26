bedd2b4095102fcd99a9c3839608327a8d5a8432
sethrandalldev <sethjrandall@gmail.com>
2020-12-26 11:45:50 -0800

    [ADDED] textfields for input

--- lib/screens/bookshelf/bookshelf.dart
+++ lib/screens/bookshelf/bookshelf.dart
@@ -2,7 +2,28 @@ import 'package:bookshelf/bookshelf_list.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Bookshelf extends StatelessWidget {
+class Bookshelf extends StatefulWidget {
+  @override
+  _BookshelfState createState() => _BookshelfState();
+}
+
+class _BookshelfState extends State<Bookshelf> {
+
+String _title = '';
+String _author = '';
+
+  void _setTitle(title) {
+    setState(() {
+      _title = title;
+    });
+  }
+
+  void _setAuthor(author) {
+    setState(() {
+      _author = author;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -11,21 +32,40 @@ class Bookshelf extends StatelessWidget {
 
     Future<void> addBook() {
       return books.add({
-        'title': 'Ready Player One',
-        'author': 'Ernest Cline'
+        'title': _title,
+        'author': _author
       })
       .then((value) => print('Book added'))
       .catchError((error) => print('Failed to add user: $error'));
     }
 
-    return Column(
-      children: [
-        TextButton(
-          onPressed: addBook,
-          child: Text('Add Book')
-        ),
-        Expanded(child: Card(child: BookshelfList()))
-      ]
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: Scaffold(body: 
+        Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Book Title'
+              ),
+              onChanged: (String value) => _setTitle(value)
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Book Author'
+              ),
+              onChanged: (String value) => _setAuthor(value)
+            ),
+            TextButton(
+              onPressed: addBook,
+              child: Text('Add Book')
+            ),
+            Expanded( child: BookshelfList())
+          ]
+        )
+      ),
     );
   }
 }
